21ea82b30ff2fde59b532c5fd2b2a7f1c16a4a87
solen0id <max.patzelt@gmail.com>
2017-04-13 15:09:13 +0200

    default to PIL Image when cv2 can't create image from array

--- doc2text/page.py
+++ doc2text/page.py
@@ -2,37 +2,44 @@ import pyocr
 import io
 
 import cv2
+import PIL
 import numpy as np
 from PIL import Image
 
 from . import transformations
 
 
-def get_cv_image_from_file(fd):
+def get_image_from_file(fd):
     try:
         image_array = np.asarray(bytearray(fd.read()), dtype=np.uint8)
         return cv2.imdecode(image_array, 0)
     except:
         return Image.open(io.BytesIO(bytearray(fd.read())))
 
-def cv_to_pil(cv_im):
-    return Image.fromarray(cv_im)
+def cv_to_pil(image_array):
+    if isinstance(image_array,  PIL.Image.Image):
+        return image_array
+    else:
+        return Image.fromarray(image_array)
 
 
 class Page(object):
     def __init__(self, file_descriptor):
-        self.original = get_cv_image_from_file(file_descriptor)
+        self.original = get_image_from_file(file_descriptor)
         self.pyocr_tool = pyocr.get_available_tools()[0]
         self._processed = None
 
     def maybe_rotate(self):
         image = cv_to_pil(self.original)
         try:
-            rotation_degree = self.pyocr_tool.detect_orientation(image, lang='deu')['angle']
+            theta = self.pyocr_tool.detect_orientation(image, lang='deu')['angle']
         except:
-            rotation_degree = 0.
-        theta = rotation_degree
-        return transformations.rotate(self.original, theta)
+            theta = 0.
+
+        if isinstance(image, PIL.Image.Image):
+            return image.rotate(theta)
+        else:
+            return transformations.rotate(self.original, theta)
 
     def extract_text(self, lang):
         image = cv_to_pil(self.processed)
@@ -45,10 +52,10 @@ class Page(object):
     @property
     def processed(self):
         if not self._processed:
+            rotated_image = self.maybe_rotate()
             try:
-                rotated_image = self.maybe_rotate()
                 cropped_image = transformations.process_image(rotated_image)
                 self._processed = transformations.process_skew(cropped_image)
                 return self._processed
             except:
-                return self.original
+                return rotated_image
